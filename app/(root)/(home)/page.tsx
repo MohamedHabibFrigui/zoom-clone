6331b2935ce99166aed1985f149f5264ee9f3093
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-03-30 15:51:48 +0100

    Add home cards

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,5 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
+
 const HomePage = () => {
   const now = new Date();
 
@@ -23,6 +25,7 @@ const HomePage = () => {
           </div>
         </div>
       </div>
+      <MeetingTypeList />
     </section>
   );
 };
